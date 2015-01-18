fae4eb48f481eedcaaf03d044b2cc9895ee50323
Emon Khan <emon@khan.com>
2015-01-18 14:14:23 +0600

    delete unused classes

--- Demo.Core/Infrastructure/PageableListQueryCommand.cs
+++ Demo.Core/Infrastructure/PageableListQueryCommand.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Linq;
-using Demo.Core.ExtensionMethods;
+//using Demo.Core.ExtensionMethods;
 
 namespace Demo.Core.Infrastructure
 {
     [Serializable]
-    public class PageableListQueryCommand<TDomainObject> :IValidatableObject
+    public class PageableListQueryCommand<TDomainObject> 
         where TDomainObject : class
     {
         private readonly IEnumerable<string> sortOrders = new[] { "asc", "desc" };
@@ -28,34 +28,7 @@ namespace Demo.Core.Infrastructure
 
         public int Limit { get; set; }
 
-        public virtual PageableListQueryCommand<TDomainObject>
-            MapSortProperties(params object[] columns)
-        {
-            if (columns == null)
-            {
-                throw new ArgumentNullException("columns");
-            }
-
-            foreach (var column in columns)
-            {
-                var stringColumn = column as string;
-
-                if (stringColumn != null)
-                {
-                    this.SetSorting(stringColumn, stringColumn);
-                }
-                else
-                {
-                    foreach (var pair in column.ToDictionary())
-                    {
-                        this.SetSorting(pair.Key, pair.Value.ToString());
-                    }
-                }
-            }
-
-            return this;
-        }
-
+       
         public virtual string GetOrderByClause()
         {
             if (string.IsNullOrWhiteSpace(this.Sort))
@@ -95,57 +68,5 @@ namespace Demo.Core.Infrastructure
             return sortProperty + " " + sort[1];
         }
 
-        public IEnumerable<ValidationResult> Validate(
-           ValidationContext validationContext)
-        {
-            if (!string.IsNullOrWhiteSpace(this.Sort))
-            {
-                var sort = this.Sort.Split(
-                   new[] { ' ' },
-                   StringSplitOptions.RemoveEmptyEntries);
-
-                if (sort.Length > 1 &&
-                    !this.sortOrders.Contains(sort[1], StringComparer.OrdinalIgnoreCase))
-                {
-                    var sortOrderErrorMessage = string.Format(
-                        CultureInfo.CurrentCulture,
-                        "Invalid sort order, only supports {0} orders.",
-                        string.Join(", ", this.sortOrders));
-
-                    yield return new ValidationResult(sortOrderErrorMessage);
-                }
-            }
-
-            if (this.Offset < 0)
-            {
-                yield return new ValidationResult(
-                    "Offset must be a positive integer.",
-                    new[] { "Offset" });
-            }
-
-            //if ((this.Limit < 1) || (this.Limit > 100))
-            //{
-            //    yield return new ValidationResult(
-            //        "Limit must be between 1-100.",
-            //        new[] { "Limit" });
-            //}
-        }
-
-        private void SetSorting(string alias, string target)
-        {
-            var map = this.sortColumMappings.FirstOrDefault(row =>
-                row.Item1.Equals(target, StringComparison.OrdinalIgnoreCase));
-
-            if (map != null)
-            {
-                throw new ArgumentException(
-                    "Mapping already exists for \"" +
-                    alias +
-                    "\".",
-                    "alias");
-            }
-
-            this.sortColumMappings.Add(new Tuple<string, string>(alias, target));
-        }
     }
 }
